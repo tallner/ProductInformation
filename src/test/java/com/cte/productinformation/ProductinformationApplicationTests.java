25b4a60a8260dc424295d848f2ee1f0e98b86364
tallner <79505011+tallner@users.noreply.github.com>
2021-04-21 21:56:39 +0200

    Baseproject created

---
+++ src/test/java/com/cte/productinformation/ProductinformationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cte.productinformation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductinformationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
