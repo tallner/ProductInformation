d856402a8c401f69ecc969293ea77bd9425e90b4
tallner <79505011+tallner@users.noreply.github.com>
2021-04-23 21:50:07 +0200

    def_commit

--- src/main/java/com/cte/productinformation/ProductinformationController.java
+++ src/main/java/com/cte/productinformation/ProductinformationController.java
@@ -1,6 +1,6 @@
 package com.cte.productinformation;
 
-import java.util.ArrayList;
+//import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
